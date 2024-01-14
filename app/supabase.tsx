15c26e33790d94c22b8b3f39e0e19984a536d45d
Sameer Faisal <imrtls00@gmail.com>
2024-01-14 11:03:03 +0500

    Update Supabase key to use environment variable

--- app/supabase.tsx
+++ app/supabase.tsx
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://mvahhnvsfrgsqjjwbsph.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12YWhobnZzZnJnc3Fqandic3BoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUxNjkwMzUsImV4cCI6MjAyMDc0NTAzNX0.YkJiaQ9aR3VMVfWi3o5cP650V4dt4vzclY5yCuROsvo'
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export { supabase }
